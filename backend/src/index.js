3c9c2d1d391b22df7cc600e48166fc3aed9f0afb
Aditya Verma <meaditya14.verma@gmail.com>
2026-03-07 01:38:47 +0530

    Navigation Bug Solved, New Page Added for Instructor (404 for now)

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,9 +5,15 @@ import testRoutes from "./routes/test.route.js";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 dotenv.config();
+
+app.use(cors({
+  origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+  credentials: true,
+}));
 
 app.use(express.json());
 app.use(cookieParser());
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { login, logout, signup } from "../controllers/auth.controller.js";
+import { checkAuth, login, logout, signup } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 const router = express.Router();
 
 router.post("/signup", signup);
@@ -7,5 +8,7 @@ router.post("/signup", signup);
 router.post("/login", login);
 
 router.post("/logout", logout);
+
+router.get("/check", protectRoute, checkAuth);
 
 export default router;
